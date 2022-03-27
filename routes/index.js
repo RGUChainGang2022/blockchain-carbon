6710dfd1bc5643332b71220c1fd116e2364a3bb9
Conor <git@dbuidl.com>
2022-03-27 11:45:07 +0100

    Prevhash fix

--- routes/index.js
+++ routes/index.js
@@ -81,7 +81,7 @@ async function getBlocks(peer) {
                             let previousBlockHash = "0000000000000000000000000000000000000000000000000000000000000000";
 
                             if (block.number > 0) {
-                                previousBlockHash = sha256Hash.update(JSON.stringify(previousBlock.block)).digest("hex");
+                                previousBlockHash = sha256Hash.update(JSON.stringify(previousBlock)).digest("hex");
                             }
 
                             console.log("Previous block hash:", previousBlockHash);
